a7ae6e0535f7f39423e38ee242da7fc096bf3cfc
CatoNator <jesse.j.koivisto@hotmail.com>
2015-03-26 19:20:57 +0200

    Kamera paevitus + jotain muuta

---
+++ Defence of Cogs/Assets/Script/CameraBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public float x = 0f;
+	public float z = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			z += Input.GetAxisRaw ("Vertical") * 0.2f;
+			x += Input.GetAxisRaw ("Horizontal") * 0.2f;
+
+		transform.position = new Vector3(x, 10, z);
+	
+	}
+}
