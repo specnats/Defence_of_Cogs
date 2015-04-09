f68f13d384ac4e9826a6f8fc46065cb822fb81da
CatoNator <jesse.j.koivisto@hotmail.com>
2015-04-09 19:31:45 +0300

    toimi pls

--- Defence of Cogs/Assets/Script/CameraBehaviour.cs
+++ Defence of Cogs/Assets/Script/CameraBehaviour.cs
@@ -5,6 +5,9 @@ public class CameraBehaviour : MonoBehaviour {
 
 	public float x = 0f;
 	public float z = 0f;
+	public float y = 20f;
+
+	private float translationSpeed = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +17,26 @@ public class CameraBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-			z += Input.GetAxisRaw ("Vertical") * 0.2f;
-			x += Input.GetAxisRaw ("Horizontal") * 0.2f;
+		//fix this so it will move according to x rotation so we can have rotation with the camera too and produce interesting and noy boring camrera angles.	
+		float verticalMovement = Input.GetAxisRaw ("Vertical") * 7f;
+		float horizontalMovement = Input.GetAxisRaw ("Horizontal") *7f;
+		float cameraZoomChange = Input.GetAxisRaw ("CameraY") * 5f;
 
-		transform.position = new Vector3(x, 20, z);
+		//z += verticalMovement;
+		//x += horizontalMovement;
+		//y += cameraZoomChange;
+
+
+		Vector3 camZoom = transform.up;
+		Vector3 camHorizontal = transform.right;
+		Vector3 camVertical = transform.forward;
+
+		//transform.position = new Vector3(x, y, z);
+		transform.Translate (camZoom * cameraZoomChange * Time.deltaTime);
+		transform.Translate (camHorizontal * horizontalMovement * Time.deltaTime);
+		transform.Translate (camVertical * verticalMovement * Time.deltaTime);
+
+		//transform.for
 	
 	}
 }
