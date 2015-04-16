bab77ba5e8366341d804e4aa1fdf15d94a9707fb
CatoNator <jesse.j.koivisto@hotmail.com>
2015-04-16 18:59:30 +0300

    Jotain.com

---
+++ Defence of Cogs/Assets/Script/MousePosRaycastTes.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePosRaycastTes : MonoBehaviour {
+
+	public GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if Input.GetButton("Fire1")
+		{
+			RaycastHit rHit = new RaycastHit();
+			Ray rRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(rRay, out rHit, 1000))
+			{
+				Debug.Log("Got a hit!");
+				//Screen.showCursor
+			}
+
+		}*/
+	
+	}
+}
