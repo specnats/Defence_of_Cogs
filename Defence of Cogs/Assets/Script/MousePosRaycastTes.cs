bbb594f02f5ad1919568805f105926a1dbe864b4
Matias Koksinen <matias.koskinen@hotmail.com>
2015-04-16 19:55:20 +0300

    pilvessä ollaan

--- Defence of Cogs/Assets/Script/MousePosRaycastTes.cs
+++ Defence of Cogs/Assets/Script/MousePosRaycastTes.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MousePosRaycastTes : MonoBehaviour {
 
 	public Camera mainCamera;
+	public GameObject tower;
 
 	// Use this for initialization
 	void Start () {
@@ -13,13 +14,15 @@ public class MousePosRaycastTes : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetButton("Fire1"))
+		if(Input.GetButtonDown("Fire1"))
 		{
 			RaycastHit rHit = new RaycastHit();
 			Ray rRay = mainCamera.ScreenPointToRay(Input.mousePosition);
 			if(Physics.Raycast(rRay, out rHit, 1000))
 			{
 				Debug.Log("Got a hit!");
+				GameObject instance = Instantiate(tower);
+				instance.transform.position = rHit.point;
 				//Screen.showCursor
 			}
 
