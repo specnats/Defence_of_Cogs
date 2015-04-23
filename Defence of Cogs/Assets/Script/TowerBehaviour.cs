50b8d346defc40924815ef8795d2dde98f1ee2a5
CatoNator <jesse.j.koivisto@hotmail.com>
2015-04-23 20:14:19 +0300

    hyvin jännä

--- Defence of Cogs/Assets/Script/TowerBehaviour.cs
+++ Defence of Cogs/Assets/Script/TowerBehaviour.cs
@@ -14,14 +14,37 @@ public class TowerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		transform.LookAt (enemyPlaceholder.transform.position);
-	
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		if (enemies.Length > 0) 
+		{ 
+			// find the enemy closest to the tower
+			GameObject closest = enemies[0];
+			float distToClosest = (closest.transform.position - transform.position).sqrMagnitude;
+			foreach(GameObject enemy in enemies)
+			{
+				float distToCurrent  = (enemy.transform.position - transform.position).sqrMagnitude;
+				if (distToCurrent < distToClosest)
+				{
+					closest = enemy;
+					distToClosest = distToCurrent;
+				}
+			}
+			transform.LookAt (closest.transform.position);
+			transform.rotation = Quaternion.Euler(new Vector3(0, transform.eulerAngles.y, 0));
+		}
+
+		if(Input.GetButton("Fire2"))
+		{
+			if (enemies.Length > 0) 
+			{
+				GameObject bullet = Instantiate (projectile);
+				bullet.GetComponent<ProjectileBehaviour>().target = enemies[0];
+			}
+		}
 	}
-
-	void OnTriggerEnter () {
-
+	void OnTriggerEnter (Collider other)
+	{
 		GameObject bullet = Instantiate (projectile);
-		projectile.transform.position = transform.position;
-		projectile.transform.forward = transform.forward;
+		bullet.GetComponent<ProjectileBehaviour> ().target = other.gameObject;
 	}
 }
